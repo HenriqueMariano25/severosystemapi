0ea4db629e936eab886e5092fbd4d9daf19b5e4e
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2022-01-07 15:02:47 -0300

    Aumenta limitação de arquivos que pode ser enviado para o Multer

--- src/routes/laudoRouter.js
+++ src/routes/laudoRouter.js
@@ -7,7 +7,7 @@ routes.post('/laudo', LaudoController.cadastrar)
 
 routes.put('/laudo/:id/finalizar', LaudoController.finalizar)
 
-routes.put("/laudo/:id", multer(multerConfig).array('files', 4), LaudoController.editar)
+routes.put("/laudo/:id", multer(multerConfig).array('files', 30), LaudoController.editar)
 
 routes.get("/laudos", LaudoController.buscarTodos)
 
