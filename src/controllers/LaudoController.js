ffab91287bca1a611ea7154e6c9772f9c1a5a081
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2022-01-07 10:44:48 -0300

    Corrigir erro ao salvar imagem de um veiculo Salvo e Continuado

--- src/controllers/LaudoController.js
+++ src/controllers/LaudoController.js
@@ -115,117 +115,99 @@ class LaudoController {
                 status_laudo_id: 1
             })
 
-            return res.status(200).json({laudo_id})
+            return res.status(200).json({laudo_id, veiculo_id})
         } catch (e) {
             console.log(e)
-            return res.status(400).json({message: 'Erro ao cadastrar laudo'})
+            return res.status(400).json({mensagem: 'Erro ao cadastrar laudo'})
         }
 
 
     }
 
     async editar(req, res) {
-        let dados = JSON.parse(req.body.data)
-        let {laudo_id} = dados
+        try {
+            let dados = JSON.parse(req.body.data)
+            let {laudo_id} = dados
 
-        let questoes = dados.questoes
-        let id_questoes = []
-        for (let key in questoes) {
-            if (questoes[key]['LaudoQuestao'] === undefined) {
-                let {id} = await LaudoQuestao.create({laudo_id, questao_id: questoes[key].id})
-                id_questoes.push(id)
-            } else {
-                id_questoes.push(questoes[key]['LaudoQuestao']['id'])
+            let questoes = dados.questoes
+            let id_questoes = []
+            for (let key in questoes) {
+                if (questoes[key]['LaudoQuestao'] === undefined) {
+                    let {id} = await LaudoQuestao.create({laudo_id, questao_id: questoes[key].id})
+                    id_questoes.push(id)
+                } else {
+                    id_questoes.push(questoes[key]['LaudoQuestao']['id'])
+                }
             }
-        }
 
-        await LaudoQuestao.destroy({where: {[Op.and]: [{id: {[Op.not]: id_questoes}}, {laudo_id: laudo_id}]}})
+            await LaudoQuestao.destroy({where: {[Op.and]: [{id: {[Op.not]: id_questoes}}, {laudo_id: laudo_id}]}})
 
-        let imgs = dados.imgs
+            let imgs = dados.imgs
 
 
-        let id_imgs = []
-        for (let img of imgs) {
-            if (img.id !== undefined)
-                id_imgs.push(img.id)
-        }
+            let id_imgs = []
+            for (let img of imgs) {
+                if (img.id !== undefined)
+                    id_imgs.push(img.id)
+            }
 
-        let imgsParaDeletar = await ImagemLaudo.findAll({where: {[Op.and]: [{id: {[Op.not]: id_imgs}}, {laudo_id: laudo_id}]}})
+            let imgsParaDeletar = await ImagemLaudo.findAll({where: {[Op.and]: [{id: {[Op.not]: id_imgs}}, {laudo_id: laudo_id}]}})
 
-        for (let img of imgsParaDeletar) {
-            if (process.env.STORAGE_TYPE === "s3") {
-                const s3 = new aws.S3({
-                    accessKeyId: AWS_ACCESS_KEY_ID,
-                    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-                    region: AWS_DEFAULT_REGION
-                })
-                s3.deleteObject({
-                    Bucket: process.env.BUCKET_NAME,
-                    Key: img.nome
-                })
-            } else {
-                fs.unlink((path.resolve(__dirname, '..', '..', 'tmp', 'uploads', img.nome)),
-                    function (err) {
-                        if (err) throw err;
+            for (let img of imgsParaDeletar) {
+                if (process.env.STORAGE_TYPE === "s3") {
+                    const s3 = new aws.S3({
+                        accessKeyId: AWS_ACCESS_KEY_ID,
+                        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+                        region: AWS_DEFAULT_REGION
                     })
+                    s3.deleteObject({
+                        Bucket: process.env.BUCKET_NAME,
+                        Key: img.nome
+                    })
+                } else {
+                    fs.unlink((path.resolve(__dirname, '..', '..', 'tmp', 'uploads', img.nome)),
+                        function (err) {
+                            if (err) throw err;
+                        })
+                }
+                ImagemLaudo.destroy({where: {id: img.id}})
+
             }
-            ImagemLaudo.destroy({where: {id: img.id}})
+            const files = req.files
 
-        }
-        const files = req.files
+            for (let key in files) {
+                let peca_veiculo_id = req.body.peca_veiculo_id[key]
+                let nomeFormatado = `${dayjs().format('DDMMYYYYHHmmssSSS')}-${files[key].originalname}`
+                let url = ''
+                let nome = nomeFormatado
 
-        for (let key in files) {
-            let peca_veiculo_id = req.body.peca_veiculo_id[key]
-            let nomeFormatado = `${dayjs().format('DDMMYYYYHHmmssSSS')}-${files[key].originalname}`
-            let url = ''
-            let nome = nomeFormatado
+                if (process.env.STORAGE_TYPE === 's3') {
+                    const originalFile = files[key]
 
-            if (process.env.STORAGE_TYPE === 's3') {
-                const originalFile = files[key]
+                    const newFile = await sharpify(originalFile)
 
-                const newFile = await sharpify(originalFile)
+                    let data = await uploadToAWS({
+                        Body: newFile,
+                        ACL: 'public-read',
+                        Bucket: process.env.BUCKET_NAME,
+                        ContentType: originalFile.mimetype,
+                        Key: `${nomeFormatado}`
+                    })
+                    url = data['Location']
+                    nome = data['Key']
 
-                let data = await uploadToAWS({
-                    Body: newFile,
-                    ACL: 'public-read',
-                    Bucket: process.env.BUCKET_NAME,
-                    ContentType: originalFile.mimetype,
-                    Key: `${nomeFormatado}`
-                })
-                url = data['Location']
-                nome = data['Key']
+                } else if (process.env.STORAGE_TYPE === 'local') {
+                    await sharp(files[key].buffer).toFile(path.resolve('tmp/uploads/', nomeFormatado))
+                }
 
-            } else if (process.env.STORAGE_TYPE === 'local') {
-                await sharp(files[key].buffer).toFile(path.resolve('tmp/uploads/', nomeFormatado))
+                if (!url) {
+                    url = `${req.protocol}://${req.get('host')}/files/${nome}`
+                }
+
+                await ImagemLaudo.create({url, nome: nome, laudo_id, peca_veiculo_id})
             }
 
-            if (!url) {
-                url = `${req.protocol}://${req.get('host')}/files/${nome}`
-            }
-
-            await ImagemLaudo.create({url, nome: nome, laudo_id, peca_veiculo_id})
-        }
-
-        let {
-            placa,
-            ano,
-            hodometro,
-            uf,
-            cidade,
-            marca_modelo,
-            chassi_bin,
-            motor_bin,
-            cor_bin,
-            combustivel,
-            renavam,
-            crlv,
-            tipo_lacre,
-            cambio_bin,
-            lacre,
-            quilometragem
-        } = dados.veiculo
-
-        let {id: veiculo_id} = await Veiculo.update({
+            let {
                 placa,
                 ano,
                 hodometro,
@@ -241,26 +223,42 @@ class LaudoController {
                 tipo_lacre,
                 cambio_bin,
                 lacre,
-                quilometragem,
-                tipo_veiculo_id: dados.veiculo.tipo_veiculo.id
-            },
-            {where: {id: laudo_id}})
+                quilometragem
+            } = dados.veiculo
 
-        let {
-            nome_razao_social: prop_nome,
-            cpf_cnpj: prop_cpf_cnpj,
-            cnh: prop_cnh,
-            telefone: prop_telefone,
-            email: prop_email
-        } = dados.proprietario
+            let {id: veiculo_id} = await Veiculo.update({
+                    placa,
+                    ano,
+                    hodometro,
+                    uf,
+                    cidade,
+                    marca_modelo,
+                    chassi_bin,
+                    motor_bin,
+                    cor_bin,
+                    combustivel,
+                    renavam,
+                    crlv,
+                    tipo_lacre,
+                    cambio_bin,
+                    lacre,
+                    quilometragem,
+                    tipo_veiculo_id: dados.veiculo.tipo_veiculo.id
+                },
+                {where: {id: dados.veiculo.id}})
+            let {
+                nome_razao_social: prop_nome,
+                cpf_cnpj: prop_cpf_cnpj,
+                cnh: prop_cnh,
+                telefone: prop_telefone,
+                email: prop_email
+            } = dados.proprietario
 
-        let {situacao, observacao, perito: perito_id, perito_auxiliar: perito_auxiliar_id} = dados.resumo
+            let {situacao, observacao, perito: perito_id, perito_auxiliar: perito_auxiliar_id} = dados.resumo
 
-        let {id: cliente_id} = dados.cliente
+            let {id: cliente_id} = dados.cliente
 
-        let laudo
-
-        try {
+            let laudo
             await Laudo.update({
                     cliente_id,
                     prop_nome,
@@ -282,11 +280,11 @@ class LaudoController {
                 }).then(result => {
                 laudo = result[1]
             })
+            return await res.status(200).json({laudo: laudo})
         } catch (e) {
             console.log(e)
-            return res.status(400).json({'message': 'Erro ao editar laudo'})
+            return res.status(400).json({mensagem: 'Erro ao editar laudo'})
         }
-        return res.status(200).json({laudo: laudo})
     }
 
 
