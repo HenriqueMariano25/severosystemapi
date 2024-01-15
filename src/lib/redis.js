bdaea67a11e305343658edcd317cfd180dc65266
HenriqueMariano25 <henrique.mariano25@hotmail.com>
2024-01-15 07:53:26 -0300

    Tenta corrigir erro de não encontrar o Redis

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -3,7 +3,7 @@ const Redis = require("ioredis")
 class redis {
   constructor() {
     this.redis = new Redis({
-      url: process.env.REDIS_URL
+      url: process.env.NODE_ENV === 'development' ? 'localhost:6379' : 'redis://default:LkqA44ZqLrUYyYw8gdulsnKoOCMiROGI@redis-11400.c74.us-east-1-4.ec2.cloud.redislabs.com:11400'
       // keyPrefix: process.env.REDIS_HOST || 6379,
     })
   }
